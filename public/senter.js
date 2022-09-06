3a7b61af560c6739221e715345a5de0a24092b9d
Sreejith KS <sreejithksgupta2255@gmail.com>
2022-09-06 13:05:13 +0530

    init 2

--- public/senter.js
+++ public/senter.js
@@ -1,7 +1,7 @@
 document.getElementById('senternamebutton').addEventListener('click',function(){
     var name = document.getElementById('snameipt').value;
     if(name.length > 0){
-    var url = 'http://127.0.0.1:5500/index.html?name='+name;
+    var url = 'https://greetings5559.web.app/index.html?name='+name;
     //share the url in whatsapp
     var text = 'Share this link with your friends to send them a message: '+url;
     var whatsappurl = 'whatsapp://send?text='+text;
