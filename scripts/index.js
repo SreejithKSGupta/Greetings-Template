d0173fcc4e2e83c5ee5720f293510cecc5dcd83b
sreejith ks <sreejithksgupta2255@gmail.com>
2024-06-26 11:21:35 +0530

    update 2.2 better grammer

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     const url = new URL(window.location.href);
-    const name = url.searchParams.get('name') || 'we';
+    const name = url.searchParams.get('name') + "wishes" || 'we wish';
     document.getElementById('sentername').textContent = name;
     
     document.getElementById('sentyours').addEventListener('click', () => {
-        window.location.href = 'sender.html';
+        window.location.href = './sender.html';
     });
 });
--- scripts/sender.js
+++ scripts/sender.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const name = document.getElementById('snameipt').value;
         if (name.trim().length > 0) {
             const url = `https://sreejithksgupta.github.io/Greetings-Template/?name=${encodeURIComponent(name)}`;
-            const text = `Share this link with your friends to send them a message: ${url}`;
+            const text = `Share this link with your friends: ${url}`;
             const whatsappUrl = `whatsapp://send?text=${encodeURIComponent(text)}`;
             window.location.href = whatsappUrl;
         } else {
